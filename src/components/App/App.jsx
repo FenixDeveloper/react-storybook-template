ef41ce3fc36a690c300236bf3dcc453a97848145
Maria Gromyko <mazvereva91@mail.ru>
2023-09-06 16:42:49 +0300

    husky install

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,9 +6,7 @@ function App() {
 		<div className={styles.App}>
 			<header className={styles.App_header}>
 				<img src={logo} className={styles.App_logo} alt="logo" />
-				<p>
-					старт проекта
-				</p>
+				<p>старт проекта</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
